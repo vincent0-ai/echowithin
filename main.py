5efc90497a811d391117bec6c3aae80728575ac2
Vinny <vincento041@gmail.com>
2025-11-09 18:21:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,122 @@
+# Import necessary modules needed
+
+from flask import Flask, request, jsonify, render_template, url_for, redirect, session, flash
+from flask_login import LoginManager, UserMixin, login_required, logout_user, current_user
+import os
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson.objectid import ObjectId
+from secret import SECRET 
+from flask_bootstrap import Bootstrap4
+
+ # Initialise Flask
+app = Flask(__name__)
+
+# Initialise Bootstrap
+#bootstrap = Bootstrap4(app)
+
+#Initialise the Login Manager
+login_manager = LoginManager(app)
+
+#setup the secret key
+app.config["SECRET_KEY"] = SECRET
+
+#setup and initialise the mongodb database
+client = MongoClient('localhost', 27017)
+db = client['hotspot']
+users_conf = db['users']
+
+
+
+# Currently i don't know what this class and the next function is doing.
+# I just figured out that it is essential for the program to run.
+###################### START #################################
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data["_id"])
+        self.username = user_data["username"]
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_data = users_conf.find_one({"_id": ObjectId(user_id)})
+    return User(user_data) if user_data else None
+######################## STOP ####################################
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username and password:
+            user = users_conf.find_one({'username': username})
+            if user:
+                flash("Try using a different username")
+                return render_template("register.html")
+            else:
+                password = generate_password_hash(password)
+                user = users_conf.insert_one({
+                    'username' : username,
+                    'password' : password
+                })
+                flash("Proceed to log in")
+            return redirect(url_for("login"))
+        else:
+            flash('Username and password are required')
+            return render_template("register.html")
+    return render_template("register.html")
+    
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        
+        user = users_conf.find_one({"username": username})
+        if user and password and check_password_hash(user["password"], password):
+            session['user_id'] = str(user['_id'])
+            session['username'] = user['username']
+            return redirect(url_for('home'))
+        else:
+            flash("Wrong details provided")
+    return render_template("login.html")
+
+
+@app.route('/')
+@app.route('/dashboard')
+def dashboard():
+    if current_user.is_authenticated:
+        username=session.get('username')
+        return render_template("home.html", username=username)
+    return render_template("dashboard.html")
+
+@app.route('/home')
+def home():
+    if current_user.is_authenticated:
+        username=session.get('username')
+        return render_template("home.html", username=username)
+    return redirect(url_for("dashboard"))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
+
+
+# Handles any possible errors
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for("dashboard")), 404
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+

b0cfb2dce2e3acc9a015657a0a28fbccfcd5bec8
Vinny <petertom13249@gmail.com>
2026-07-23 20:50:25 +0300

    Fix crashing that was happening because of the missing imports

--- blueprints/bonds.py
+++ blueprints/bonds.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, render_template, current_app, url_for
+from flask import Blueprint, request, jsonify, render_template, current_app, url_for, session
 from flask_login import login_required, current_user
 from bson.objectid import ObjectId
 import datetime
